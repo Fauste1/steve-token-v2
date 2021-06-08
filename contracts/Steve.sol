cce0b2d458e9c628ef5e042ec1af59f6b9f7e63b
Fauste1 <stepan.fau@gmail.com>
2021-06-08 23:26:34 +0200

    ownable, pausable functionality

--- contracts/Steve.sol
+++ contracts/Steve.sol
@@ -2,15 +2,31 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Steve is ERC20 {
-    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) ERC20(name_, symbol_) {
-      _mint(msg.sender, initialSupply_);
+// Should I explicitly declare the interface here?
+
+
+contract Steve is ERC20, Ownable, Pausable {
+  constructor(string memory name_, string memory symbol_, uint256 initialSupply_) ERC20(name_, symbol_) {
+    _mint(msg.sender, initialSupply_);
   }
 
-    // Make the contract ownable, set owner upon deployment
+  function mintFurther(uint amount) external onlyOwner {
+    _mint(msg.sender, amount);
+  }
 
-    // Add the minting function, make it available to the owner
+  function pauseToken() external onlyOwner {
+    _pause();
+  }
 
-    // Make the token pausable by the owner
+  function unpauseToken() external onlyOwner {
+    _unpause();
+  }
+
+  function transfer(address recipient, uint256 amount) public virtual override whenNotPaused returns (bool) {
+    _transfer(_msgSender(), recipient, amount);
+    return true;
+  }
 }
